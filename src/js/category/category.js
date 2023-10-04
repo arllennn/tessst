7e9d49b14ba4df01673263ba7b7c6ae34b893d19
Arlen Usonkariev <ausenkariev@gmail.com>
2023-10-05 01:20:28 +0600

    select category, section info

--- src/js/category/category.js
+++ src/js/category/category.js
@@ -3,31 +3,37 @@ const data = [
         img: '/src/img/bike 3 (3).png',
         title: 'Look 979 BLACK FLUO YELLOW GREEN XT 2x11S AMC 2018',
         price: '649 110 ₽',
+        button: 'В 1 клик',
     },
     {
         img: '/src/img/image 23 (1).png',
-        title: 'Scott Scale 700 SL 2016',
+        title: 'Trek Dual Sport 2 Mulsanne Hybd 2022',
         price: '629 100 ₽',
+        button: 'В 1 клик',
     },
     {
         img: '/src/img/image 23 (1).png',
-        title: 'Trek Marlin 4 Aloha ATB 27.5 2022',
+        title: 'Trek Marlin 4 Aloha ATB 27.5 2022 YELLOW',
         price: '73 300 ₽',
+        button: 'В 1 клик',
     },
     {
         img: '/src/img/image 23 (1).png',
-        title: 'Orbea RUDE 10 Черный 2022',
+        title: 'Look 977 BLACK FLUO YELLOW GREEN XT 2x11S AMC 2018',
         price: '89 100 ₽',
+        button: 'В 1 клик',
     },
     {
         img: '/src/img/image 23 (1).png',
         title: 'Trek Marlin 5 Lithium Grey Chrome ATB 27.5 2022',
         price: '84 499 ₽',
+        button: 'В 1 клик',
     },
     {
         img: '/src/img/image 23 (1).png',
         title: 'Trek Dual Sport 2 Mulsanne Hybd 2022',
         price: '90 968 ₽',
+        button: 'В 1 клик',
     }
 ]
 
@@ -41,17 +47,20 @@ const category = (data) => {
         const categoryImg = document.createElement('img')
         const categoryTitle = document.createElement('h3')
         const categoryPrice = document.createElement('p')
+        const categoryBtn = document.createElement('button')
 
         categoryImg.src = el.img
         
         categoryTitle.textContent = el.title
         categoryPrice.textContent = el.price
+        categoryBtn.textContent = el.button
 
         categoryImg.className = 'category-img'
         categoryTitle.className = 'category-title'
         categoryPrice.className = 'category-price'
+        categoryBtn.className = 'category-btn'
 
-        categoryBox.append(categoryImg, categoryTitle, categoryPrice)
+        categoryBox.append(categoryImg, categoryTitle, categoryPrice, categoryBtn)
         blockCategoryRight.append(categoryBox)
     })
 }
@@ -64,7 +73,24 @@ const categoryInput = document.querySelector('.block__category--left__input')
 const search = () => {
     categoryInput.addEventListener('input', () => {
         const filterData = data.filter(item => item.title.toLowerCase().includes(categoryInput.value.toLowerCase()));
-        category(filterData)
-    })
-}
-search()
+        category(filterData);
+    });
+};
+search();
+
+const categorySelect = document.querySelector('.block__category--left__select');
+
+const select = () => {
+    categorySelect.addEventListener('change', () => {
+       if(categorySelect){
+        const selectValue = categorySelect.value;
+        const newFilterData = data.filter(item => item.price === selectValue);
+        category(newFilterData);
+       }
+       if(categorySelect.value === 'категории по ценам'){
+        category(data)
+       }
+    });
+};
+
+select();
