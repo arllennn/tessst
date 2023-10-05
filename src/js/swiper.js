dfcfb3eb410937cb20d4e7b5a94c021a64f4ab98
Arlen Usonkariev <ausenkariev@gmail.com>
2023-10-05 16:57:53 +0600

    section models

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -3,6 +3,9 @@ const swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  pagination: {
+    el: ".swiper-pagination",
+  },
   breakpoints: {
     0: {
         slidesPerView:1,
