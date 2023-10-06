b8558a46f28a1adf278676e8fceca715684f1e80
Arlen Usonkariev <ausenkariev@gmail.com>
2023-10-06 16:26:47 +0600

    acardeon css

--- src/js/acardeon/acardeon.js
+++ src/js/acardeon/acardeon.js
@@ -5,6 +5,7 @@ const acardeonBottom = document.querySelectorAll('.block__acardeon--content__bot
 acardeonContent.forEach(el => {
     el.addEventListener('click', () => {
         el.classList.toggle('block__acardeon--content__active')
+        el.classList.toggle('block__acardeon--content__active--top__wrap--svg__active')
     })
 })
 acardeonBottom.forEach(el => {
