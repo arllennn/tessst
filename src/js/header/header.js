d88c579e520b263d8429cfcc0cdcb6df240f72cf
Arlen Usonkariev <ausenkariev@gmail.com>
2023-10-08 14:24:19 +0600

    qr code

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -5,3 +5,11 @@ backdrop.addEventListener('click', () => {
     nav.classList.toggle('navActive')
     backdrop.classList.toggle('navActive')
 })
+const qrcodeBtn = document.querySelector('.block__header--contacts__btn')
+const qrcode = document.querySelector('.qrcode')
+qrcodeBtn.addEventListener('click', () => {
+    qrcode.classList.toggle('qrcode__active')
+    setTimeout(() => {
+        qrcode.classList.remove('qrcode__active')
+    }, 11000)
+})
